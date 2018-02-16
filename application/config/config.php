54ca095d6161e20d99058e20b79a75bc04f4ae05
one4two <petyaburka@gmail.com>
2018-02-16 13:27:21 +0200

    Changes config.php

--- application/config/config.php
+++ application/config/config.php
@@ -22,13 +22,4 @@
 	define('DB_PASS','');
 	define('DB_CHARSET','utf8');
 	
-	// test
-/*
-	define('URL_PUBLIC_FOLDER', 'public');
-	define('URL_PROTOCOL', '//');
-	define('URL_DOMAIN', $_SERVER['HTTP_HOST']);
-	define('URL_SUB_FOLDER', str_replace(URL_PUBLIC_FOLDER, '', dirname($_SERVER['SCRIPT_NAME'])));
-	define('URL', URL_PROTOCOL . URL_DOMAIN . URL_SUB_FOLDER);
-*/
-
 ?>
